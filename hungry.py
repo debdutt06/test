6c9b8c8f070717b0c510cdf1b39cfde4c9384cff
debdutt06 <60596973+debdutt06@users.noreply.github.com>
2025-01-13 16:08:19 +0530

    changes made

--- hungry.py
+++ hungry.py
@@ -1,6 +1,7 @@
 print("hello welcome to Restursnt")
 print("###########################")
 print("@@@@@@@@@@@@@@@@@@@@")
+print("555555555555555555555")
 hungry=input("are you hungry?")
 if hungry=="yes":
     print("eat samosa")
